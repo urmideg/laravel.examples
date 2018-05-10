d901f2017e083bf623affa07050473721d342ed5
Ivan Leonov <leonov.k13@yandex.ru>
2018-05-10 20:51:00 +0300

    Extending A Layout
    Определение маршрута для представления. Наследование макета. Внедрение содержимое в секцию макета.

---
+++ resources/views/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="jumbotron col-md-6 offset-md-3 text-white bg-dark mb-3 text-center">
+      <h1 class="display-4">Laravel 5.6</h1>
+      <hr class="my-2">
+      <p class="lead ">Рассматриваются базовые понятия использования компонентов фреймворка</p>
+      <hr class="my-4">
+      <p class="lead">
+        <a class="btn btn-lg btn-outline-secondary" href="https://laravel.com/docs/5.6" role="button">Документация на английском языке</a>
+        <a class="btn btn-lg btn-outline-secondary" href="http://laravel.su/docs/5.4" role="button">Документация на русском языке</a>
+      </p>
+    </div>
+
+@endsection
